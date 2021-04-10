41aafa4663c60dc3990bd162e4e74f59ec8a3485
BrianCKWang <brian.ck.wang@gmail.com>
2021-04-10 18:54:59 -0400

    remove thought when associated user is deleted

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,4 +1,5 @@
-const { User } = require('../models');
+const { User, Thought } = require('../models');
+const { db } = require('../models/User');
 
 const UserController = {
   // get all Users
@@ -68,15 +69,23 @@ const UserController = {
 
   // delete User
   deleteUser({ params }, res) {
-    User.findOneAndDelete({ _id: params.id })
-      .then(dbUserData => {
-        if (!dbUserData) {
-          res.status(404).json({ message: 'No user found with this id!' });
-          return;
-        }
-        res.json(dbUserData);
+    User.findOne({ _id: params.id })
+    .then(dbUserData => {
+      dbUserData.thoughts.forEach(thoughtId => {
+        Thought.findOneAndDelete({ _id: thoughtId });
       })
-      .catch(err => res.status(400).json(err));
+    })
+    .then(() => {
+      return User.findOneAndDelete({ _id: params.id });
+    })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id!' });
+        return Promise.reject();
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => res.status(400).json(err));
   },
 
   addUserFriend({ params }, res) {
